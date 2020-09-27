513685ae27aaf82e44ecfb0f92c23fade30298fa
ilham aliyudin <ilhampti3@gmail.com>
2020-09-27 18:42:14 +0700

    new database

--- config/db_hiring_app.sql
+++ config/db_hiring_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2020 at 01:40 PM
+-- Generation Time: Sep 24, 2020 at 09:14 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -44,12 +44,7 @@ CREATE TABLE `engineer` (
 INSERT INTO `engineer` (`id`, `nama`, `jobdesc`, `lokasi`, `status`, `deskripsi`, `skill`) VALUES
 (22, 'Ryunoesuke', 'Web Developer', 'Surabaya', 'Freelancer', 'Native Mobile Programmer with Kotlin', 'PHP'),
 (24, 'ilham aliyudin', 'Web Developer', 'Bandung', 'Freelancer', 'Native Mobile Programmer with Kotlin', 'Kotlin'),
-(26, 'Lionel messsiah', 'Web Developer', 'Surabaya', 'Freelancer', 'Native Mobile Programmer with Kotlin', 'PHP'),
-(27, 'Christian Ronaldo', 'Sys Admin', 'Jakarta', 'Freelancer', 'Junior Sys Admins', 'Linux'),
-(28, 'Pogba', 'Sys Admin', 'Jakarta', 'Freelancer', 'Junior Sys Admins', 'Linux'),
-(29, 'Manuel Neur', 'Sys Admin', 'Jakarta', 'Freelancer', 'Junior Sys Admins', 'Linux'),
-(30, 'Lewadownski', 'Devs Ops', 'Banten', 'Fulltime', 'Junior Devs Ops', 'Linux'),
-(31, 'Gotze', 'Sys Admin', 'Jakarta', 'Freelancer', 'Junior Sys Admins', 'Linux');
+(26, 'Lionel messsiah', 'Web Developer', 'Surabaya', 'Freelancer', 'Native Mobile Programmer with Kotlin', 'PHP');
 
 -- --------------------------------------------------------
 
@@ -61,17 +56,10 @@ CREATE TABLE `experience` (
   `id` int(30) NOT NULL,
   `company_name` varchar(20) NOT NULL,
   `position` varchar(20) NOT NULL,
-  `start_periode` varchar(20) NOT NULL,
-  `end_periode` varchar(20) NOT NULL,
-  `description` varchar(200) NOT NULL
+  `masuk` varchar(20) NOT NULL,
+  `keluar` varchar(20) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `experience`
---
-
-INSERT INTO `experience` (`id`, `company_name`, `position`, `start_periode`, `end_periode`, `description`) VALUES
-(1, 'PT MAHA RAJA', 'SYS ADMIN', '20 Agustus 2011', '17 Agustus 2017', 'No Description');
 
 -- --------------------------------------------------------
 
@@ -83,18 +71,10 @@ CREATE TABLE `portfolio` (
   `id` int(30) NOT NULL,
   `nama_app` varchar(100) NOT NULL,
   `link_github` varchar(100) NOT NULL,
+  `type_porfolio` varchar(100) NOT NULL,
   `deskripsi` varchar(100) NOT NULL,
-  `image` text NOT NULL
+  `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `portfolio`
---
-
-INSERT INTO `portfolio` (`id`, `nama_app`, `link_github`, `deskripsi`, `image`) VALUES
-(5, 'bedah', 'Disini link githubnya', 'Ini Deskripsi', 'image-1601184894510-apis-for-marketers.png'),
-(9, 'bedah', 'Disini link githubnya', 'Ini Deskripsi', 'image-1601185207654-Screenshot (7).png'),
-(10, 'bedah', 'Disini link githubnya', 'Ini Deskripsi', 'image-1601186163675-Screenshot (7).png');
 
 -- --------------------------------------------------------
 
@@ -146,17 +126,9 @@ INSERT INTO `recruiter` (`id`, `nama`, `bidang`, `lokasi`, `deskripsi`, `email`,
 --
 
 CREATE TABLE `skill` (
-  `id_skill` int(30) NOT NULL,
+  `id` int(30) NOT NULL,
   `nama_skill` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `skill`
---
-
-INSERT INTO `skill` (`id_skill`, `nama_skill`) VALUES
-(1, ''),
-(2, 'kotlin');
 
 -- --------------------------------------------------------
 
@@ -167,21 +139,13 @@ INSERT INTO `skill` (`id_skill`, `nama_skill`) VALUES
 CREATE TABLE `user` (
   `id` int(100) NOT NULL,
   `user_name` varchar(100) NOT NULL,
-  `user_password` varchar(200) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
   `user_email` varchar(100) NOT NULL,
   `user_role` int(1) NOT NULL,
   `user_status` int(1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `user_name`, `user_password`, `user_email`, `user_role`, `user_status`, `created_at`, `updated_at`) VALUES
-(11, 'ilham aliyudin', '$2a$10$Pycye8nF6ddr4xfhsUx2FetBA7ZIMmF.G3nIIj3Q9GRLiSIIVOYGy', 'ilham@gmail.com', 1, 1, '2020-09-26 12:55:39', '2020-09-26 12:55:39'),
-(13, 'ilham aliyudin', '$2a$10$Q38KVqh.64Ygm1KmDPNQJ.vTdegSc7wXXD1swtrtOl2JDyMj.oG/m', 'ilham@gmail.com', 1, 1, '2020-09-26 13:38:03', '2020-09-26 13:38:03');
 
 --
 -- Indexes for dumped tables
@@ -221,7 +185,7 @@ ALTER TABLE `recruiter`
 -- Indexes for table `skill`
 --
 ALTER TABLE `skill`
-  ADD PRIMARY KEY (`id_skill`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -237,19 +201,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `engineer`
 --
 ALTER TABLE `engineer`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `experience`
 --
 ALTER TABLE `experience`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `portfolio`
 --
 ALTER TABLE `portfolio`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `project`
@@ -267,13 +231,13 @@ ALTER TABLE `recruiter`
 -- AUTO_INCREMENT for table `skill`
 --
 ALTER TABLE `skill`
-  MODIFY `id_skill` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
